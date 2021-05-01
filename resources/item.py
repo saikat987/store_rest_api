6128d7ec3649aafc4a15c1a556956c28d69df143
saikat987 <saikatdebnath12@gmail.com>
2021-05-01 16:57:05 +0530

    all files togather

---
+++ resources/item.py
@@ -0,0 +1,70 @@
+from flask_restful import Resource,reqparse
+from flask_jwt import jwt_required
+import sqlite3
+from models.item import ItemModel
+
+class Item(Resource):
+    parser=reqparse.RequestParser()
+    parser.add_argument('price',
+    type=float,
+    required=True,
+    help='this field cannot be blank')
+    parser.add_argument('store_id',
+    type=int,
+    required=True,
+    help='need a store id')
+
+    @jwt_required()
+    def get(self,name):
+        item=ItemModel.find_by_name(name)
+        if item:
+            return item.json()
+        return {'message':'no match found'}
+
+
+
+    def post(self,name):
+        if ItemModel.find_by_name(name):
+            return {'message':'an item already existed'},400
+
+        data=Item.parser.parse_args()
+
+        item=ItemModel(name,**data)
+
+        try :
+            item.save_to_db()
+        except:
+            return {'message':"an error occured inserting the item"},500
+        return item.json(),201
+
+
+
+
+    def delete(self,name):
+        item=ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+        else:
+            return {'message':'no item found with that name'}
+        return {'message':'item has been deleted'}
+    def put(self,name):
+        data = Item.parser.parse_args()
+        item = ItemModel.find_by_name(name)
+
+        if item is None:
+            item=ItemModel(name,**data)
+
+        else:
+            item.price=data['price']
+            item.store_id=data['store_id']
+        item.save_to_db()
+        return item.json()
+
+
+
+
+class ItemList(Resource):
+    TABLE_NAME = 'items'
+
+    def get(self):
+        return {'items':list(map(lambda x:x.json(),ItemModel.query.all()))}
